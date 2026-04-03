36a86ddaafdea7408427b08bdbde1b7858c425a4
anujpatel002 <swaminarayan2181@gmail.com>
2026-04-03 19:24:12 +0530

    Rename WelcomePage to LoginPage and update references in root.dart

--- Login/root.dart
+++ Login/root.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'LoginPage.dart';   // ✅ fixed
 import 'welcome.dart';
 
+void main() {
+  runApp(const Root());
+}
+
 class Root extends StatelessWidget {
+  const Root({super.key});
+
   @override
   Widget build(BuildContext context) {
 
